6215fabb760a88bce285623a841b8618cb42d096
S-Spiegl <steven.spiegl@gmail.com>
2022-10-17 12:17:00 +0100

    works for pre-ordered list regardless of length

--- removeDuplicates.test.js
+++ removeDuplicates.test.js
@@ -4,4 +4,7 @@ describe('removeDuplicates', () => {
   it('removes duplicates from pre-ordered array', () => {
     expect(removeDuplicates([1,2,2])).toEqual([1,2])
   })  
+  it('removes duplicates from pre-ordered array regardless of length', () => {
+    expect(removeDuplicates([1,2,2,5,6,8,8,10,10,11])).toEqual([1,2,5,6,8,10,11])
+  })
 })
